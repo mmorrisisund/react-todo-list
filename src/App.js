73fb21be7452c2e5cc6428ba38561cd3baee9a9a
MiMo <michael.a.morris.public@gmail.com>
2020-07-21 22:21:20 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { v4 as uuid } from 'uuid'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <div className='row'>
+          <TodoInput />
+          <TodoList />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/TodoItem.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+  render () {
+    return <h1>hello from todo item</h1>
+  }
+}
